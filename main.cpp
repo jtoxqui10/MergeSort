3ead37e142bc0b0ed02271547994c8fc6f78262e
Jonatan Toxqui <jtoxqui001@gmail.com>
2017-10-16 19:41:25 -0700

    added main.cpp

--- main.cpp
+++ main.cpp
@@ -3,32 +3,60 @@
 
 using namespace std;
 
-void msort(int a[], int x[], int s, int e) {
-	if(e - s < 1) return;
-	int m = (s + e) / 2;
-	/*msort(a, x, s, m)
-	msort(a, x, m + 1, e)*/
-	int i = s;
-	int j = m + 1;
-	int k;
-	int index;
-	while(i <= m && j <= e) {
-		if(a[i] <= a[j]) {
-			a[index++] = x[i++];
-		}
+void msort(int a[], int x[], int s, int e) 
+{
+    if(e - s < 1) return;
+    int m = (s + e) / 2;
+    int i, j, k;
+    i = s;
+    j = m +1;
+    k = 0;
 
-		else {
-			a[index++] = x[j++];
-		}
-	}
+    while (i <= m && j <= e) {
+        if (a[i] < a[j]) {
+            x[k++] = a[i++];
+        }
+        else {
+            x[k++] = a[j++];
+        }
+    }
+    while (i <= m) {
+        x[k++] = a[i++];
+    }
+    while (j <= e) {
+        x[k++] = a[j++];
+    }
+    for (i = s; i <= e; i++) {
+        a[i] = x[i - s];
+    }
 }
 
-/*void mergesort(int a[], int n) {
-	int x[] = new int[len];
-	msort(a, x, 0, len-1);
-	delete []x;
-}
+    
 
+
+void mergesort(int a[], int n) {
+    int len;
+    n = len;
+    int * x = new int[len];
+    msort(a, x, 0, len - 1);
+    delete []x;
+
+}
 bool sorted(int a[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        if (a[i] > a[i + 1]) {
+            return false;
+        }
+    }
+}
 
-} */
+int main() {
+    int a[] = {3, 2, 5, 21, 1};
+    mergesort(a,5);
+    assert(sorted(a,5));
+
+    cout << "All tests passed." << endl;
+    
+}
+
+
