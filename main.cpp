409c058ad8cc46c95a7ecf2ccf545828aced196c
Jonatan Toxqui <jtoxqui001@gmail.com>
2017-10-16 20:54:00 -0700

    added main.cpp

--- main.cpp
+++ main.cpp
@@ -3,14 +3,16 @@
 
 using namespace std;
 
-void msort(int a[], int x[], int s, int e) 
-{
+void msort(int a[], int x[], int s, int e) {
     if(e - s < 1) return;
     int m = (s + e) / 2;
     int i, j, k;
     i = s;
     j = m +1;
     k = 0;
+
+	msort(a, x, s, m);
+	msort(a, x, m + 1, e);
 
     while (i <= m && j <= e) {
         if (a[i] < a[j]) {
@@ -35,10 +37,8 @@ void msort(int a[], int x[], int s, int e)
 
 
 void mergesort(int a[], int n) {
-    int len;
-    n = len;
-    int * x = new int[len];
-    msort(a, x, 0, len - 1);
+    int * x = new int[n];
+    msort(a, x, 0, n - 1);
     delete []x;
 
 }
@@ -47,16 +47,31 @@ bool sorted(int a[], int n) {
         if (a[i] > a[i + 1]) {
             return false;
         }
+	
+
     }
+   return true;
 }
 
-int main() {
-    int a[] = {3, 2, 5, 21, 1};
-    mergesort(a,5);
-    assert(sorted(a,5));
+int main(int argc, char * args[]) {
+	int a[1000];
+	for (int i = 0; i < 1000; ++i) a[i] = -50 + rand() % 100;
+	mergesort(a, 1000);
+	assert(sorted(a, 1000));
 
-    cout << "All tests passed." << endl;
-    
+	int b[1001];
+	for (int i = 0; i < 1001; ++i) b[i] = -50 + rand() % 100;
+	mergesort(b, 1001);
+	assert(sorted(b, 1001));
+
+	int c[] = { 2 };
+	mergesort(c, 1);
+	assert(sorted(c, 1));
+	int d[] = { 1, 2, 3, 4, 5 };
+	mergesort(d, 5);
+	assert(sorted(d, 5));
+
+	cout << "All tests passed." << endl;    
 }
 
 
